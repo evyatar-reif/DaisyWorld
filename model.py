6e8d760df57c22167b5d794c125c59f22e1604e1
evyatar-reif <evyatar.reif@gmail.com>
2024-07-03 21:01:57 +0300

    new graphs

--- model.py
+++ model.py
@@ -22,7 +22,6 @@ def plot_daisies(daisies):
     plt.xlabel('Luminosity')
     plt.ylabel('Area %')
     plt.legend()
-    plt.gca().invert_xaxis()
     plt.ylim(0, 100)  # Set y-axis limits from 0 to 100
 
 
@@ -34,31 +33,31 @@ def plot_world_temp(temp_data):
     plt.title('WORLD TEMPERATURES')
     plt.xlabel('Luminosity')
     plt.ylabel('Temperature C')
-    plt.gca().invert_xaxis()
     plt.legend()
 
 
 ### SIMULATION
 
 MAX_DAYS = 2000
-END_L = 0.5
-START_L = 1.7
-L_STEP = -0.05
+END_L = 1.7
+START_L = 0.6
+L_STEP = 0.05
 DEFAULT_AREA = 0.01
 
 white = Daisy("white", 0.75, DEFAULT_AREA)
 black = Daisy("black", 0.25, DEFAULT_AREA)
-yellow = Daisy("yellow", 0.6, DEFAULT_AREA)
 purple = Daisy("purple", 0.4, DEFAULT_AREA)
+yellow = Daisy("yellow", 0.6, DEFAULT_AREA)
 
 
-daisies = [white, black, yellow, purple]
+
+daisies = [white, black, purple, yellow]
 
 temp_data = []
 l_data = []
 
 L = START_L
-while L > END_L:
+while L < END_L:
     simulate_l(daisies, L, MAX_DAYS, DEFAULT_AREA)
 
     l_data.append(L)
